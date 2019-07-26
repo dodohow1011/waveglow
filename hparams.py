26bc94675758876959e958d881c7b43f819247b0
willytai43 <b04502139@ntu.edu.tw>
2019-07-26 17:22:12 +0800

    hparams

--- hparams.py
+++ hparams.py
@@ -34,10 +34,10 @@ def create_hparams(hparams_string=None, verbose=False):
         # Audio Parameters             #
         ################################
         max_wav_value=32768.0,
-        sampling_rate=16000,
+        sampling_rate=22050,
         filter_length=1024,
-        hop_length=200,
-        win_length=800,
+        hop_length=256,
+        win_length=1024,
         n_mel_channels=80,
         mel_fmin=0.0,
         mel_fmax=8000.0,
